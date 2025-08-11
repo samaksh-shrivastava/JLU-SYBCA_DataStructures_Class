eee5f402a878ff30e6abf3f2fadf4a6106aa4867
samaksh-shrivastava <samakshshrivastava60@gmail.com>
2025-08-11 21:43:34 +0530

    Create program10.cpp

---
+++ program10.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int* arr = new int[size];
+
+    cout << "Enter " << size << " elements:\n";
+    for(int i = 0; i < size; i++){
+        cin >> arr[i];
+    }
+
+    cout << "You entered: ";
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    delete[] arr;
+    return 0;
+}
